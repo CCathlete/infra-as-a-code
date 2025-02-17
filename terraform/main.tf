30c4bdf67da45d7a1043ca257b1cd3cc2199b3ba
ccat <ccathlete01@gmail.com>
2025-02-17 22:19:41 +0100

    About to start creating the vagrantfile and ansible playbook.

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    vagrant = {
+      source  = "bmatcuk/vagrant"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
+resource "vagrant_vm" "ubuntu_vm" {
+  name            = "vagrantbox"
+  vagrantfile_dir = "../vagrant"
+  get_ports       = true
+}
+
